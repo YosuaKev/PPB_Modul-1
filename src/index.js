d14847037967cafeea68089f214451111abf6b43
Yosua <yosuakevan@gmail.com>
2025-09-24 12:21:16 +0700

    Searching Medications By Name 6

--- src/index.js
+++ src/index.js
@@ -3,14 +3,26 @@ import dotenv from "dotenv";
 import medicationRoutes from "./routes/medicationRoutes.js";
 import categoryRoutes from "./routes/categoryRoutes.js";
 import supplierRoutes from "./routes/supplierRoutes.js";
+
 dotenv.config();
 const app = express();
 app.use(express.json());
+
+// 🔥 Middleware anti-cache
+app.use((req, res, next) => {
+  res.setHeader("Cache-Control", "no-store, no-cache, must-revalidate, proxy-revalidate");
+  res.setHeader("Pragma", "no-cache");
+  res.setHeader("Expires", "0");
+  res.setHeader("Surrogate-Control", "no-store");
+  next();
+});
+
 // routes
 app.use("/api/suppliers", supplierRoutes);
 app.use("/api/categories", categoryRoutes);
 app.use("/api/medications", medicationRoutes);
+
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
-console.log(`Server running on port ${port}`);
+  console.log(`Server running on port ${port}`);
 });
